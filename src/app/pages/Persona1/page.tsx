66fad91af776141b6f5b5f87ac1db8754958ecc1
Jose <josemaurette1234@gmail.com>
2024-11-04 19:04:15 -0500

    Fixed Persona1

--- src/app/pages/Persona1/page.tsx
+++ src/app/pages/Persona1/page.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <h1>Persona:</h1>
-      <h2>Persona 1</h2>
-      <h2>Persona 2</h2>
-      <h2>Persona 3</h2>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;

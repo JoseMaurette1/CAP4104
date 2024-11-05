5e3394312549830929eb4d67d08afe7cad61d6d3
Jose <josemaurette1234@gmail.com>
2024-11-05 17:30:44 -0500

    Coded TrailTales Flowchart page

--- src/app/pages/FlowChart/page.tsx
+++ src/app/pages/FlowChart/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Chart from "@/app/assets/Chart.png";
+import Navbar from "@/app/_components/Navbar";
+import Logo from "@/app/_components/Logo";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Navbar />
+      <Logo />
+      <div className="min-h-screen flex items-center justify-center flex-col">
+        <h1 className="pb-2 font-bold text-white text-2xl">
+          TrailTales Flowchart:
+        </h1>
+        <Image src={Chart} alt="FlowChart" width={800} height={300} />
+      </div>
+    </>
+  );
 };
 
 export default page;

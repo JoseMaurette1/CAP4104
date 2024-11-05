8fc943262d9d761ba16b0ea471fe6af38ada1ac4
Jose <josemaurette1234@gmail.com>
2024-11-05 18:29:12 -0500

    Made TrailTales Intro Page

--- src/app/Introduction/page.tsx
+++ src/app/Introduction/page.tsx
@@ -1,28 +1,29 @@
 import React from "react";
+import StyledText from "../_components/StyledText";
+import Image from "next/image";
+import TT from "@/app/assets/TT.png";
 
 const page = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-20">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        className="mb-4 border-4 border-white rounded"
+        src={TT}
+        alt="Logo"
+        width={400}
+        height={300}
+      ></Image>
       <div className="w-full max-w-4xl px-6">
-        <h1 className="font-bold text-2xl text-center">
-          TrailTales Introduction:
-        </h1>
-        <p>
-          TrailTales is a web-based platform for outdoor enthusiasts who want to
-          document their hiking and camping experiences through pictures and
-          trip logs. Unlike social media platforms, TrailTales focuses on
-          creating a personal, organized archive of hikes and camping trips,
-          allowing users to upload images and detailed descriptions of trails,
-          scenery, and camping experiences. The platform emphasizes simplicity
-          and functionality without social media interactions like comments,
-          likes, or follower counts.
+        <p className="pt-4 text-xl">
+          <StyledText>TrailTales</StyledText> is a web-based platform for
+          outdoor enthusiasts who want to document their hiking and camping
+          experiences through pictures and trip logs. Unlike social media
+          platforms, TrailTales focuses on creating a personal, organized
+          archive of hikes and camping trips, allowing users to upload images
+          and detailed descriptions of trails, scenery, and camping experiences.
+          The platform emphasizes simplicity and functionality without social
+          media interactions like comments, likes, or follower counts.
         </p>
-
-        <div className="flex flex-col items-center justify-center mt-4">
-          <h2> Similar Products:</h2>
-          <p>AllTrails</p>
-          <p>Outdoor Project</p>
-        </div>
       </div>
     </div>
   );

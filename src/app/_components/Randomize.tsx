7fdc637182bc213b686792f85a57693701f3070b
Jose <josemaurette1234@gmail.com>
2024-11-05 20:03:21 -0500

    TO-DO* Sorting ALGORITHM

---
+++ src/app/_components/Randomize.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Randomize = () => {
+  return (
+    <>
+      {/* Take pics that user uploads and develop sorting algorithm to randomize them in different positions and sizes */}
+    </>
+  );
+};
+
+export default Randomize;

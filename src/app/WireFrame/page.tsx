6b03aa53a46c0710f212dc619fa8e08dcbc3c246
Jose <josemaurette1234@gmail.com>
2024-11-05 20:22:43 -0500

    To-Do

--- src/app/WireFrame/page.tsx
+++ src/app/WireFrame/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import StyledText from "../_components/StyledText";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <StyledText>To be Continued...</StyledText>
+      {/* Work on WireFrame */}
+    </div>
+  );
 };
 
 export default page;

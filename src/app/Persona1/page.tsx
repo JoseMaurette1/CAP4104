0bf03068a39cbf8197ab6b3c95ad8024d2353dc9
Jose <josemaurette1234@gmail.com>
2024-11-05 19:15:39 -0500

    Finished Persona1

--- src/app/Persona1/page.tsx
+++ src/app/Persona1/page.tsx
@@ -1,7 +1,39 @@
-import Navbar from "@/app/_components/Navbar";
+import React from "react";
+import StyledText from "../_components/StyledText";
 
 const page = () => {
-  return <div></div>;
+  const Joe = {
+    name: "Joe",
+    age: 30,
+    interests: ["Hiking", "Photography", "Camping"],
+    bio: "Joe is an outdoor enthusiast who loves filming his outdoor adventures.",
+    goals:
+      "Joe wants a website where he can post his adventures to keep them as a record without having to worry about social media.",
+    painpts:
+      "He dislikes social media and all the interactions that come with it and prefers just to be able to document his journeys and post pictures.",
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="border border-2 p-4 hover:border-dashed  transform transition-transform duration-300 hover:translate-y-[-4px]">
+        <h1 className="text-center">
+          Persona 1: <StyledText>{Joe.name}</StyledText>
+        </h1>
+        <div className="mt-4 text-center">
+          <h2 className="text-xl">Age: {Joe.age}</h2>
+          <h3 className="mt-2 text-xl">Interests:</h3>
+          <ul className="list-disc list-inside mt-2 text-center text-xl ">
+            {Joe.interests.map((interest, index) => (
+              <li key={index}>{interest}</li>
+            ))}
+          </ul>
+          <p className="pt-4 text-gray-400 text-xl"> {Joe.bio}</p>
+          <p className="pt-2 text-gray-400 text-xl"> {Joe.goals}</p>
+          <p className="pt-2 text-gray-400 text-xl"> {Joe.painpts}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;

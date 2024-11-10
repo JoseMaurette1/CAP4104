a2241986464c9bccb13f25d12264534220bb79c9
Jose <josemaurette1234@gmail.com>
2024-11-10 18:43:57 -0500

    Finished WireFrame page

--- src/app/WireFrame/page.tsx
+++ src/app/WireFrame/page.tsx
@@ -1,11 +1,70 @@
 import React from "react";
-import StyledText from "../_components/StyledText";
+import wireframe from "@/app/assets/wireframe.png";
+import Image from "next/image";
+import GridOnIcon from "@mui/icons-material/GridOn";
+import Box from "@mui/material/Box";
+import SettingsIcon from "@mui/icons-material/Settings";
+import TerrainIcon from "@mui/icons-material/Terrain";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ExploreIcon from "@mui/icons-material/Explore";
+import CommentIcon from "@mui/icons-material/Comment";
+import HikingIcon from "@mui/icons-material/Hiking";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
 
 const page = () => {
+  const icons = [
+    <SettingsIcon />,
+    <GridOnIcon />,
+    <TipsAndUpdatesIcon />,
+    <TerrainIcon />,
+    <HikingIcon />,
+    <CommentIcon />,
+    <BookmarkIcon />,
+    <CameraAltIcon />,
+    <AccountBoxIcon />,
+    <ArrowBackIcon />,
+    <ExploreIcon />,
+  ];
+  const colors = [
+    "#582F03",
+    "#7F4F24",
+    "#936639",
+    "#C2C5AA",
+    "#A4AC86",
+    "#656D4A",
+    "#414833",
+    "#333D29",
+  ];
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <StyledText>To be Continued...</StyledText>
-      {/* Work on WireFrame */}
+    <div className="min-h-screen flex items-center flex-col justify-center">
+      <h1 className="font-bold p-10">Example Wire Frame:</h1>
+      <Image src={wireframe} alt="pic" height={1000} width={1000} />
+      <h1 className="font-bold p-10">
+        Icons that will be used throughout the website:{" "}
+      </h1>
+      <div className="flex flex-row">
+        {icons.map((icon, index) => (
+          <ul>
+            <li key={index}>{icon}</li>
+          </ul>
+        ))}
+      </div>
+      <h1 className="font-bold p-10 text-center">
+        Theme: Explore and Share your adventure
+      </h1>
+      <h1 className="font-bold pb-5">Sample Color Palette: </h1>
+      <div className="flex flex-row space-x-4 pb-10 ">
+        {colors.map((colors, index) => (
+          <Box
+            className=" transform transition-transform duration-300 hover:translate-y-[-4px]"
+            key={index}
+            sx={{ height: 100, width: 100, bgcolor: colors }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
